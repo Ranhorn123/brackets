2708b8679730703ee1aeaae401f6a47054b99db8
Ildar <106691203+Ranhorn123@users.noreply.github.com>
2022-08-03 18:40:05 +0400

    feat:completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < bracketsConfig.length; k++) {
+      if (arr[i] == bracketsConfig[k][1] && arr[i - 1] == bracketsConfig[k][0]) {
+        arr.splice(i - 1, 2);
+        i = 0;
+        break;
+      }
+    }
+  }
+  return (arr < 1) ? true : false
 }
+
+  // your solution
+
